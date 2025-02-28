c506c96dde339bc630c21d7016ba3c1cef31559c
Harleen Kaur <gs8606197@gmail.com>
2025-02-28 19:54:00 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./configs/mongodb.js";
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
+
+//MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+
+//ROUTES
+app.get("/", (req, res) => res.send("API Working..."));
+
+app.listen(PORT, () => console.log("Server Running on port " + PORT));
