aa6eb9b94f4b90986413960dfff3c05056d2e75a
Harleen Kaur <gs8606197@gmail.com>
2025-02-28 20:31:50 +0530

    made changes

--- server/server.js
+++ server/server.js
@@ -3,22 +3,17 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 
-const PORT = process.env.PORT || 4000;
 const app = express();
 
 const startServer = async () => {
   await connectDB();
 
+  app.use(express.json());
+  app.use(cors());
 
-//MIDDLEWARES
-app.use(express.json());
-app.use(cors());
-
-//ROUTES
-app.get("/", (req, res) => res.send("API Working..."));
-
-app.listen(PORT, () => console.log("Server Running on port " + PORT));
-
+  app.get("/", (req, res) => res.send("API Working..."));
 };
 
 startServer();
+
+export default app; // Vercel requires this export
